c71b83b2b86a7c47ff9d7dfe79b8fec33e66ce3d
Rehema Wachira <rehema.wachira@andela.com>
2018-10-07 22:11:54 +0300

    Update schema

--- db/schema.rb
+++ db/schema.rb
@@ -1,14 +1,13 @@
 # frozen_string_literal: true
-ActiveRecord::Schema.define(version: 2018_10_07_135732) do
 
-  # These are extensions that must be enabled in order to support this database
+ActiveRecord::Schema.define(version: 2018_10_07_135447) do
   enable_extension "plpgsql"
 
   create_table "neighbourhoods", force: :cascade do |t|
-    t.string "name"
-    t.float "latitude"
-    t.float "longitude"
-    t.integer "radius"
+    t.string "name", null: false
+    t.float "latitude", null: false
+    t.float "longitude", null: false
+    t.integer "radius", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
