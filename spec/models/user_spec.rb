1b0785306b4f27b4abd65d4a852318bb0e29a277
Rehema Wachira <rehema.wachira@andela.com>
2018-12-01 16:43:36 +0300

    Add user model and bcrypt gem to manage passwords

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  context 'validations' do
+    subject(:user) { FactoryBot.build(:user) }
+
+    it { should validate_presence_of(:email) }
+    it { should validate_presence_of(:username) }
+    it { should validate_presence_of(:password_digest) }
+    it { should validate_presence_of(:admin) }
+
+    it { should validate_uniqueness_of(:email).case_insensitive }
+    it { should validate_uniqueness_of(:username).case_insensitive }
+
+    it { should allow_values('_user', '2user', 'use').for(:username) }
+    it { should_not allow_values('-', '/user', 'User', 'us', '16pluscharstoolong').for(:username) }
+  end
+end
