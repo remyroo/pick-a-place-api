241e0a8c29a188c1b31e7ef3217c9c6ba8be1be5
Rehema Wachira <rehema.wachira@andela.com>
2019-02-16 00:14:44 +0300

    - Update User model
    - Add index to email for quicker searching
    - Make username unique
    - Replace boolean admin field with more flexible role field

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_30_134926) do
+ActiveRecord::Schema.define(version: 2019_02_12_164715) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,9 +28,10 @@ ActiveRecord::Schema.define(version: 2018_11_30_134926) do
     t.string "email", null: false
     t.string "username", null: false
     t.string "password_digest", null: false
-    t.boolean "admin", default: false, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "role", default: "user", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
   create_table "venues", force: :cascade do |t|
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,7 +7,6 @@ RSpec.describe User, type: :model do
     it { should validate_presence_of(:email) }
     it { should validate_presence_of(:username) }
     it { should validate_presence_of(:password_digest) }
-    it { should validate_presence_of(:admin) }
 
     it { should validate_uniqueness_of(:email).case_insensitive }
     it { should validate_uniqueness_of(:username).case_insensitive }
